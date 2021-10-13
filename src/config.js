9e1e1cf84fed6af88a7f12b206ced80d2048338d
panamacheng <rangotoptalent@gmail.com>
2021-10-13 11:02:42 -0400

    update for serverless

--- src/config.js
+++ src/config.js
@@ -1,13 +1,28 @@
 import dotenv from 'dotenv';
-dotenv.config();
+import { retrieveSecretValues } from '@app/utils/aws-helper';
 
-export default {
+dotenv.config();
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+const DEV_VARIABLES = {
   NODE_ENV: process.env.NODE_ENV,
   LOG_LEVEL: process.env.LOG_LEVEL,
   APP_HOST_NAME: process.env.APP_HOST_NAME,
   PORT: process.env.PORT,
-  NGROK_AUTH_TOKEN: process.env.NGROK_AUTH_TOKEN,
   SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
   SHOPIFY_API_SECRET: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES
 };
+
+const PROD_VARIABLES = {};
+(async () => {
+  try {
+    const data = await retrieveSecretValues();
+    const variables = JSON.parse(data.SecretString);
+    console.log(variables);
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+export default PRODUCTION ? { ...PROD_VARIABLES } : { ...DEV_VARIABLES };
--- src/index.js
+++ src/index.js
@@ -5,17 +5,10 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import app from './app';
 import log from '@app/utils/log';
-import { shopifyService } from '@app/services';
-
-const PROD = process.env.NODE_ENV === 'production';
 
 process.on('uncaughtException', (ex) => {
   log.error(ex);
   process.exit(1);
 });
-
-(function () {
-  if (PROD) shopifyService.initialize();
-})();
 
 export default app;
